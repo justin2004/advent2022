d60ed52e6b8b3c0a8f5a85474569f7bc9cfc4dda
Justin <justin2004@hotmail.com>
2022-12-01 21:44:38 -0500

    day 1

---
+++ README.md
@@ -0,0 +1,1 @@
+using [apl_in_the_shell](https://github.com/justin2004/apl_in_the_shell)
---
+++ solutions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apl "{⌈/+/¨⍎¨¨⍵⊆⍨×≢¨⍵}" day1_input.txt
+apl "{+/3↑v⌷⍨⊂⍒v←+/¨⍎¨¨⍵⊆⍨×≢¨⍵}" day1_input.txt
