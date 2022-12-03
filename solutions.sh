b06d952bfafc99aa15af1591bbd475fa888e165d
Justin <justin2004@hotmail.com>
2022-12-02 21:59:46 -0500

    day2 part1

--- solutions.sh
+++ solutions.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 apl "{⌈/+/¨⍎¨¨⍵⊆⍨×≢¨⍵}" day1_input.txt
 apl "{+/3↑v⌷⍨⊂⍒v←+/¨⍎¨¨⍵⊆⍨×≢¨⍵}" day1_input.txt
+
+apl "{
+pairs←{⍵/¨⍨' '≠¨⍵}⋄
+getScore←{⍵[2]+0 3 6[⌈3÷⍨(1 3) (2 1) (3 2) (1 1) (2 2) (3 3) (1 2) (2 3) (3 1) ⍳ ⊂⍵]}⋄
+charNum←4|'ABC_XYZ'∘⍳⋄
++/getScore¨charNum¨pairs ⍵
+}" day2_input.txt
